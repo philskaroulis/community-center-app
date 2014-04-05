f26b570c313d8d250a44fd9f3c2d8d1bab98dad2
Phil Skaroulis <phil@skaroulis.com>
2014-04-05 15:24:16 -0400

    work on welcome factory

--- js/app/app.js
+++ js/app/app.js
@@ -1,28 +1,64 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('CommunityCenterApp', [
-    'ngRoute',
-    'CommunityCenterApp.filters',
-    'CommunityCenterApp.services',
-    'CommunityCenterApp.directives',
-    'CommunityCenterApp.controllers'
+angular.module( 'ccApp', [
+    'ngRoute'
 ])
-.config(['$routeProvider', function($routeProvider) {
 
-    $routeProvider.when('/', {template: 'home.html', controller: 'HomeCtrl'});
+.config( function ccAppConfig ( $routeProvider ) {
 
-    $routeProvider.when('/login', {template: 'login.html', controller: 'LoginCtrl'});
+    $routeProvider.when('/', {templateUrl: 'home.html', controller: 'HomeCtrl'});
 
-    $routeProvider.when('/content', {template: 'content.html', controller: 'ContentCtrl'});
+    $routeProvider.when('/login', {templateUrl: 'login.html', controller: 'LoginCtrl'});
 
-    $routeProvider.when('/events', {template: 'events.html', controller: 'EventsCtrl'});
+    $routeProvider.when('/welcome', {templateUrl: 'welcome.html', controller: 'WelcomeCtrl'});
 
-    $routeProvider.when('/artists', {template: 'artists.html', controller: 'ArtistsCtrl'});
+    $routeProvider.when('/history', {templateUrl: 'history.html', controller: 'HistoryCtrl'});
 
-    $routeProvider.when('/links', {template: 'links.html', controller: 'LinksCtrl'});
+    $routeProvider.when('/contact', {templateUrl: 'contact.html', controller: 'ContactCtrl'});
+
+    $routeProvider.when('/events', {templateUrl: 'events.html', controller: 'EventsCtrl'});
+
+    $routeProvider.when('/introductions', {templateUrl: 'introductions.html', controller: 'IntroductionsCtrl'});
+
+    $routeProvider.when('/links', {templateUrl: 'links.html', controller: 'LinksCtrl'});
 
     $routeProvider.otherwise({redirectTo: '/'});
 
-}]);
+})
+
+.factory( 'WelcomeFactory', function WelcomeFactory ($sce) {
+
+    // private
+    var data = {};
+    var init = {
+            title:'',
+            body:''
+        };
+
+    // public
+    return {
+        update: function(freshData){
+            data = freshData;
+            return data;
+        },
+        empty: function(){
+            data = init;
+            return data;
+        },
+        read: function(){
+            return {
+                'title': data.title,
+                'body': $sce.trustAsHtml(data.body)
+            };
+        }
+    }
+
+})
+
+.controller( 'HomeCtrl', function HomeCtrl ( $scope, $sce, WelcomeFactory ) {
+
+    $scope.welcome = WelcomeFactory.read();
+
+})
+
+;
